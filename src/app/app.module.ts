f479cfb6e5d585fe9066e62002bea951d5c5aace
autumnesponda <autumnesponda@knights.ucf.edu>
2022-01-16 13:26:26 -0500

    created site header/footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MdbCollapseModule } from "mdb-angular-ui-kit/collapse";
+import { MdbDropdownModule } from "mdb-angular-ui-kit/dropdown";
+import { MdbRippleModule } from "mdb-angular-ui-kit/ripple";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/site-header/header.component';
+import { FooterComponent } from './shared/site-footer/footer.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    GalleryComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    MdbCollapseModule,
+    MdbDropdownModule,
+    MdbRippleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
